e803b497652f883bbad4c4deaacb88bc1fbdbfb1
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2023-07-03 17:39:12 +0200

    add church data

--- src/main/java/com/campbackend/services/ChurchServices.java
+++ src/main/java/com/campbackend/services/ChurchServices.java
@@ -1,5 +1,6 @@
 package com.campbackend.services;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.campbackend.enums.ChurchType;
 import com.campbackend.input.ChurchInput;
 import com.campbackend.input.PageInput;
 import com.campbackend.modal.Church;
@@ -21,18 +23,24 @@ public class ChurchServices {
     private ChurchRepository churchRepository;
     public ResponseEntity<String> saveOrUpdateChurch(ChurchInput churchInput) {
         try {
-            Church churchData=this.findById(churchInput.getChurchId());
-           Church church=churchRepository.save(new Church(churchInput.getId(), churchInput.getName(), churchInput.getType(), churchData,churchInput.getLocation()));
+           Church churchData=new Church();
+            if(churchInput.getChurchId()!=null)
+               churchData=this.findById(churchInput.getChurchId());
+            else churchData=null;
+           Church church=churchRepository.save(new Church(churchInput.getId(), churchInput.getName(), churchInput.getType(), churchData, churchInput.getLocation()));
             return new ResponseEntity<>(church.getName() + " saved successful", HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(" Something happen", HttpStatus.METHOD_NOT_ALLOWED);
+            
         }
     }
 
     public Church findById(UUID id) {
         return churchRepository.findById(id).orElse(null);
     }
-
+    public List<Church> findAllByChurchType(ChurchType churchType) {
+        return churchRepository.findAllByType(churchType);
+    }
     public ResponseEntity<String> deleteChurch(UUID id) {
         try {
             Church church = this.findById(id);
