4c0f190366afcbddf91a9f18346ab1eea47e2437
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2023-07-03 17:38:49 +0200

    add find all by type

--- src/main/java/com/campbackend/repository/ChurchRepository.java
+++ src/main/java/com/campbackend/repository/ChurchRepository.java
@@ -1,9 +1,13 @@
 package com.campbackend.repository;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.campbackend.enums.ChurchType;
 import com.campbackend.modal.Church;
 
 public interface ChurchRepository extends JpaRepository<Church,UUID> {
+    List<Church> findAllByType(ChurchType churchType);
 
 }
