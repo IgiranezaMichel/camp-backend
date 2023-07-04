dd299afb0cde61383cbd12c40865bd070ae1cf6a
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2023-07-04 23:49:43 +0200

    add findByEmail

--- src/main/java/com/campbackend/repository/AccountHolderRepository.java
+++ src/main/java/com/campbackend/repository/AccountHolderRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.campbackend.modal.AccountHolder;
 
 public interface AccountHolderRepository extends JpaRepository<AccountHolder,UUID>{
-
+    AccountHolder findByEmail(String id);
 }
