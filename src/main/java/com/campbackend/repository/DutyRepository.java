ed52de8e4e073038f7f5993fd7b5b0aca8d77841
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2023-07-05 08:45:06 +0200

    add find all by church

--- src/main/java/com/campbackend/repository/DutyRepository.java
+++ src/main/java/com/campbackend/repository/DutyRepository.java
@@ -1,9 +1,14 @@
 package com.campbackend.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.campbackend.modal.Church;
 import com.campbackend.modal.Duty;
 public interface DutyRepository extends JpaRepository<Duty,UUID> {
+
+    List<Duty> findAllByChurch(Church church);
 
 }
