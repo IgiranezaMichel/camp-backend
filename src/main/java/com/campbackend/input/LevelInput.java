a8e1d586701d2a0cbaf7e773b1c008ab525c6418
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-07 17:03:25 +0200

    add LevelInput

---
+++ src/main/java/com/campbackend/input/LevelInput.java
@@ -0,0 +1,11 @@
+package com.campbackend.input;
+
+import com.campbackend.modal.Levels;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class LevelInput extends Levels{
+@Getter @Setter
+private String base64Photo;
+}
