4bf85bbdeeb49cb6b3e332ea1bb21a25ce35eb14
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-07 17:03:40 +0200

    add Base64 pic

--- src/main/java/com/campbackend/modal/Levels.java
+++ src/main/java/com/campbackend/modal/Levels.java
@@ -2,9 +2,11 @@ package com.campbackend.modal;
 
 import java.util.UUID;
 
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.hibernate.annotations.UuidGenerator;
 import org.hibernate.annotations.UuidGenerator.Style;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
@@ -19,4 +21,12 @@ public class Levels {
     @Id
     @UuidGenerator(style = Style.AUTO)
     private UUID id;
+    @Column(unique=true)
+    private String name;
+    private int fromAge;
+    private int toAge;
+    private byte[] photo;
+    public String getPhoto(){
+        return Base64.encodeBase64String(photo);
+    }
 }
