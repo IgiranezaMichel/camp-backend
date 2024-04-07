3d6a6465c3656b51a085ca0ff2c3e781302e4a11
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-07 21:42:51 +0200

    add cors configuration

---
+++ src/main/java/com/campbackend/configuration/CorsConfiguration.java
@@ -0,0 +1,18 @@
+package com.campbackend.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")  
+                .allowedHeaders("*")  
+                .allowCredentials(false) 
+                .maxAge(3600);  
+    }
+}
