872d1ede935979aac6ba9b23e364d081e7b40dd1
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-08 17:11:08 +0200

    add CascadeType

--- src/main/java/com/campbackend/modal/Levels.java
+++ src/main/java/com/campbackend/modal/Levels.java
@@ -6,13 +6,15 @@ import org.apache.tomcat.util.codec.binary.Base64;
 import org.hibernate.annotations.UuidGenerator;
 import org.hibernate.annotations.UuidGenerator.Style;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import java.util.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
@@ -29,4 +31,13 @@ public class Levels {
     public String getPhoto(){
         return "data:image/png;base64,"+Base64.encodeBase64String(photo);
     }
+    public Levels(UUID id,String name,int fromAge,int toAge,byte[] photo){
+        this.setId(id);
+        this.setName(name);
+        this.setFromAge(fromAge);
+        this.setToAge(toAge);
+        this.setPhoto(photo);
+    }
+    @OneToMany(cascade = CascadeType.ALL,targetEntity = Book.class,mappedBy = "levels")
+    public List<Book> bookList;
 }
