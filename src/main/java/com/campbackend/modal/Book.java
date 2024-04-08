ecd229e6d8b9147fc69b242d228876b52b0213e4
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-08 17:10:55 +0200

    add column

--- src/main/java/com/campbackend/modal/Book.java
+++ src/main/java/com/campbackend/modal/Book.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.UuidGenerator;
 import org.hibernate.annotations.UuidGenerator.Style;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
@@ -14,6 +15,8 @@ import jakarta.persistence.ManyToOne;
 import lombok.Data;
 import lombok.*;
 import java.util.*;
+import java.time.*;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
@@ -24,19 +27,38 @@ public class Book {
     private UUID id;
     private String name;
     @Lob
-    private byte [] file;
+    private byte[] file;
     private String fileEncoding;
-    @ManyToOne(targetEntity = Levels.class,cascade = CascadeType.ALL)
+    @ManyToOne(targetEntity = Levels.class, cascade = CascadeType.ALL)
     @OnDelete(action = OnDeleteAction.SET_NULL)
     private Levels levels;
     @Lob
-    private byte [] cover;
-    public Book(UUID id,String name,String file,Levels levels,String cover){
-        this.id=id;
-        this.name=name;
-        this.file=Base64.decodeBase64(file.split("base64,")[1]);
-        this.fileEncoding=file.split("base64,")[0]+"base64,";
-        this.levels=levels;
-        this.cover=Base64.decodeBase64(cover.split("base64,")[1]);
+    private byte[] cover;
+    private String author;
+    private LocalDate publicationDate;
+    private String publisher;
+    @Column(unique = true)
+    private String serialNumber;
+
+    public String getCover() {
+        return "data:image/png;base64,"+Base64.encodeBase64String(cover);
+    }
+
+    public String getFile() {
+        return Base64.encodeBase64String(file);
+    }
+
+    public Book(UUID id, String name, String file, Levels levels, String cover, String author,
+            LocalDate publicationDate, String publisher, String serialNumber) {
+        this.id = id;
+        this.name = name;
+        this.file = Base64.decodeBase64(file.split("base64,")[1]);
+        this.fileEncoding = file.split("base64,")[0] + "base64,";
+        this.levels = levels;
+        this.cover = Base64.decodeBase64(cover.split("base64,")[1]);
+        this.author = author;
+        this.publicationDate = publicationDate;
+        this.publisher = publisher;
+        this.serialNumber = serialNumber;
     }
 }
