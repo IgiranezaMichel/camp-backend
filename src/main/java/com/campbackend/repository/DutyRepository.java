c029fdc719e04c91eb6e2de3a963844a94598799
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-11 21:14:00 +0200

    add query

--- src/main/java/com/campbackend/repository/DutyRepository.java
+++ src/main/java/com/campbackend/repository/DutyRepository.java
@@ -3,12 +3,18 @@ package com.campbackend.repository;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import com.campbackend.enums.Role;
 import com.campbackend.modal.Church;
 import com.campbackend.modal.Duty;
 public interface DutyRepository extends JpaRepository<Duty,UUID> {
 
     List<Duty> findAllByChurch(Church church);
+    @Query("SELECT d FROM Duty d WHERE d.accountHolder.role = ?1")
+    Page<Duty> findAllByAccountHolderRole(Role role, PageRequest of);
 
 }
