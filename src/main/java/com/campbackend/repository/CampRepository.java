83f668026aa2200553431078d29d3e8c103124f0
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-12 17:04:47 +0200

    add find all by ending date before

--- src/main/java/com/campbackend/repository/CampRepository.java
+++ src/main/java/com/campbackend/repository/CampRepository.java
@@ -1,11 +1,18 @@
 package com.campbackend.repository;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.campbackend.modal.Camp;
 
 public interface CampRepository extends JpaRepository<Camp,UUID> {
+
+    Page<Camp> findAllByEndingDateBefore(LocalDate now, PageRequest of);
+
+    Page<Camp> findAllByEndingDateAfter(LocalDate now, PageRequest of);
 
 }
