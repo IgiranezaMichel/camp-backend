56a7550a30845a77247c50b995770cf548fac4e4
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-14 15:57:29 +0200

    add camp mentor repository

---
+++ src/main/java/com/campbackend/repository/CampMentorRepository.java
@@ -0,0 +1,16 @@
+package com.campbackend.repository;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.campbackend.modal.AccountHolder;
+import com.campbackend.modal.Camp;
+import com.campbackend.modal.CampMentor;
+
+public interface CampMentorRepository extends JpaRepository<CampMentor,UUID>{
+    List<CampMentor> findAllByCamp(Camp camp);
+
+    CampMentor findByAccountHolderAndCamp(AccountHolder accountHolder, Camp camp2);
+}
