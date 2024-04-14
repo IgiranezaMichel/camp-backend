f3b0d27dcce187ef72ed3760928f72a5e14d523e
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-14 15:56:32 +0200

    add camp mentor list

--- src/main/java/com/campbackend/modal/Camp.java
+++ src/main/java/com/campbackend/modal/Camp.java
@@ -4,14 +4,19 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 import org.hibernate.annotations.UuidGenerator;
 import org.hibernate.annotations.UuidGenerator.Style;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.*;
 @Data
 @NoArgsConstructor
 @Entity
@@ -29,6 +34,9 @@ public class Camp {
     @Column(columnDefinition = "text")
     private String content;
     private LocalDateTime timeStamp;
+    @OneToMany(cascade = CascadeType.ALL,targetEntity = CampMentor.class,mappedBy = "camp")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private List<CampMentor>campMentorList;
     public Camp(UUID id, String title, int cost, String address, String location, LocalDate startingDate,
             LocalDate endingDate, String description, String content) {
         this.id = id;
