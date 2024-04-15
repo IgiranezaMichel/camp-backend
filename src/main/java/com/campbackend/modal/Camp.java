11694fa8fa764fd58fe6cebc9a09fb591d45fbe3
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-15 20:57:24 +0200

    add relation ship

--- src/main/java/com/campbackend/modal/Camp.java
+++ src/main/java/com/campbackend/modal/Camp.java
@@ -8,37 +8,46 @@ import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 import org.hibernate.annotations.UuidGenerator;
 import org.hibernate.annotations.UuidGenerator.Style;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.util.*;
+
 @Data
 @NoArgsConstructor
 @Entity
 public class Camp {
-    @Id @UuidGenerator(style = Style.AUTO)
+    @Id
+    @UuidGenerator(style = Style.AUTO)
     private UUID id;
     private String title;
-    private int cost; 
-    private String address; 
-    private String location; 
-    private LocalDate startingDate; 
+    private int cost;
+    private String address;
+    private String location;
+    private LocalDate startingDate;
     private LocalDate endingDate;
     private String description;
-    @ManyToOne(cascade = CascadeType.ALL,targetEntity = Levels.class)
-    private Levels levels;
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+        name = "camp_levels",
+        joinColumns = @JoinColumn(name = "camp_id"),
+        inverseJoinColumns = @JoinColumn(name = "levels_id")
+    )
+    private List<Levels> levels;
     @Column(columnDefinition = "text")
     private String content;
     private LocalDateTime timeStamp;
-    @OneToMany(cascade = CascadeType.ALL,targetEntity = CampMentor.class,mappedBy = "camp")
+    @OneToMany(cascade = CascadeType.ALL, targetEntity = CampMentor.class, mappedBy = "camp")
     @OnDelete(action = OnDeleteAction.CASCADE)
-    private List<CampMentor>campMentorList;
+    private List<CampMentor> campMentorList;
+
     public Camp(UUID id, String title, int cost, String address, String location, LocalDate startingDate,
             LocalDate endingDate, String description, String content) {
         this.id = id;
@@ -50,6 +59,6 @@ public class Camp {
         this.endingDate = endingDate;
         this.description = description;
         this.content = content;
-        setTimeStamp(LocalDateTime.now()); 
-   }
+        setTimeStamp(LocalDateTime.now());
+    }
 }
