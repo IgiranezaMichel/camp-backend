d6a1149cca23fbf3a7de9bc9131e74ad4957cfe2
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-15 20:59:35 +0200

    add campPage

--- src/main/java/com/campbackend/services/CampServices.java
+++ src/main/java/com/campbackend/services/CampServices.java
@@ -10,45 +10,63 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.campbackend.input.CampInput;
 import com.campbackend.input.PageInput;
 import com.campbackend.modal.Camp;
 import com.campbackend.pagination.CampPage;
 import com.campbackend.repository.CampRepository;
 import java.time.*;
+
 @Service
 public class CampServices {
-    @Autowired private CampRepository campRepository;
-    public ResponseEntity<String>saveOrUpdateCamp(Camp camp){
+    @Autowired
+    private CampRepository campRepository;
+    // @Autowired
+    // private LevelServices levelServices;
+    public ResponseEntity<String> saveOrUpdateCamp(CampInput camp) {
         try {
+            String ids=camp.getLevelArr();
+            System.out.println(ids.split("["));
             camp.setTimeStamp(LocalDateTime.now());
-            Camp data=campRepository.save(camp);
-            return new ResponseEntity<>(data.getTitle()+" saved successful",HttpStatus.OK);
+            Camp data = campRepository.save(camp);
+            return new ResponseEntity<>(data.getTitle() + " saved successful", HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>(" Camp already exist",HttpStatus.METHOD_NOT_ALLOWED);
+            return new ResponseEntity<>(" Camp already exist", HttpStatus.METHOD_NOT_ALLOWED);
         }
     }
-    public Camp findById(UUID id){
-      return campRepository.findById(id).orElseThrow();
+
+    public Camp findById(UUID id) {
+        return campRepository.findById(id).orElseThrow();
     }
-    public ResponseEntity<String>deleteCamp(UUID id){
+
+    public ResponseEntity<String> deleteCamp(UUID id) {
         try {
-            Camp data=this.findById(id);
+            Camp data = this.findById(id);
             campRepository.delete(data);
-            return new ResponseEntity<>(data.getTitle()+" removed successful",HttpStatus.OK);
+            return new ResponseEntity<>(data.getTitle() + " removed successful", HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>(" Camp doesn't exist",HttpStatus.METHOD_NOT_ALLOWED);
+            return new ResponseEntity<>(" Camp doesn't exist", HttpStatus.METHOD_NOT_ALLOWED);
         }
     }
-    public CampPage campPage(PageInput page){
-        org.springframework.data.domain.Page<Camp> pagination=campRepository.findAll(PageRequest.of(page.getPageNumber(),page.getPageSize(),Sort.by(page.getSort())));
-        return new CampPage(pagination.getNumber(), pagination.getTotalPages(), pagination.getTotalElements(), pagination.getContent());
-     }
-     public CampPage inActiveCamp(PageInput page){
-        org.springframework.data.domain.Page<Camp> pagination=campRepository.findAllByEndingDateBefore(LocalDate.now(),PageRequest.of(page.getPageNumber(),page.getPageSize(),Sort.by(page.getSort())));
-        return new CampPage(pagination.getNumber(), pagination.getTotalPages(), pagination.getTotalElements(), pagination.getContent());
-     }
-     public CampPage activeCamp(PageInput page){
-        org.springframework.data.domain.Page<Camp> pagination=campRepository.findAllByEndingDateAfter(LocalDate.now(),PageRequest.of(page.getPageNumber(),page.getPageSize(),Sort.by(page.getSort())));
-        return new CampPage(pagination.getNumber(), pagination.getTotalPages(), pagination.getTotalElements(), pagination.getContent());
-     }
+
+    public CampPage campPage(PageInput page) {
+        org.springframework.data.domain.Page<Camp> pagination = campRepository
+                .findAll(PageRequest.of(page.getPageNumber(), page.getPageSize(), Sort.by(page.getSort())));
+        return new CampPage(pagination.getNumber(), pagination.getTotalPages(), pagination.getTotalElements(),
+                pagination.getContent());
+    }
+
+    public CampPage inActiveCamp(PageInput page) {
+        org.springframework.data.domain.Page<Camp> pagination = campRepository.findAllByEndingDateBefore(
+                LocalDate.now(), PageRequest.of(page.getPageNumber(), page.getPageSize(), Sort.by(page.getSort())));
+        return new CampPage(pagination.getNumber(), pagination.getTotalPages(), pagination.getTotalElements(),
+                pagination.getContent());
+    }
+
+    public CampPage activeCamp(PageInput page) {
+        org.springframework.data.domain.Page<Camp> pagination = campRepository.findAllByEndingDateAfter(LocalDate.now(),
+                PageRequest.of(page.getPageNumber(), page.getPageSize(), Sort.by(page.getSort())));
+        return new CampPage(pagination.getNumber(), pagination.getTotalPages(), pagination.getTotalElements(),
+                pagination.getContent());
+    }
 }
