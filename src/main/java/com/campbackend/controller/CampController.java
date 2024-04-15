603af9d53cdee9ae6a299e0e69a521612bfe72f7
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-15 18:08:58 +0200

    add camp inputs

--- src/main/java/com/campbackend/controller/CampController.java
+++ src/main/java/com/campbackend/controller/CampController.java
@@ -8,16 +8,15 @@ import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-
+import com.campbackend.input.CampInput;
 import com.campbackend.input.PageInput;
-import com.campbackend.modal.Camp;
 import com.campbackend.pagination.CampPage;
 import com.campbackend.services.CampServices;
 @Controller
 public class CampController {
     @Autowired private CampServices campServices;
     @MutationMapping
-    public ResponseEntity<String>saveOrUpdateCamp(@Argument(name="campInput")Camp camp){
+    public ResponseEntity<String>saveOrUpdateCamp(@Argument(name="campInput")CampInput camp){
         return campServices.saveOrUpdateCamp(camp);
     }
     @MutationMapping
@@ -27,6 +26,10 @@ public class CampController {
     @QueryMapping
     public CampPage campPage(@Argument(name = "input")PageInput page){
         return campServices.campPage(page);
+    }
+    @QueryMapping
+    public CampPage inactiveCamp(@Argument(name = "input")PageInput page){
+        return campServices.inActiveCamp(page);
     }
     @QueryMapping
     public CampPage activeCamp(@Argument(name = "input")PageInput page){
