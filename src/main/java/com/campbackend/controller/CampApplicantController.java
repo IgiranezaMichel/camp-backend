fde5d152fa913b4877e6388f30585a8f52006a2e
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-15 18:08:31 +0200

    campApplicant controller

---
+++ src/main/java/com/campbackend/controller/CampApplicantController.java
@@ -0,0 +1,36 @@
+package com.campbackend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+import com.campbackend.input.CampApplicantInput;
+import com.campbackend.input.CampApplicantPage;
+import com.campbackend.input.PageInput;
+import com.campbackend.services.CampApplicantServices;
+
+@Controller
+public class CampApplicantController {
+    @Autowired
+    private CampApplicantServices campApplicantServices;
+
+    @MutationMapping
+    public ResponseEntity<String> saveOrUpdateCampApplicant(
+            @Argument(name = "campApplicantInput") CampApplicantInput campApplicantInput) {
+        return campApplicantServices.saveOrUpdateCampApplicant(campApplicantInput);
+    }
+
+    @QueryMapping
+    public CampApplicantPage campApplicantPage(@Argument(name = "input") PageInput page) {
+        return campApplicantServices.campApplicantPage(page);
+    }
+
+    @QueryMapping
+    public CampApplicantPage accountHolderCampApplicationPage(@Argument(name = "input") PageInput page,
+            @Argument(name = "email") String email) {
+        return campApplicantServices.accountHolderCampApplicationPage(page, email);
+    }
+}
