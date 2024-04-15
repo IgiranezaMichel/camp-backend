1c9b04df6ab259b58b33d400e984073f3538ae7f
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-15 18:07:49 +0200

    add comments

--- src/main/java/com/campbackend/Main.java
+++ src/main/java/com/campbackend/Main.java
@@ -1,18 +1,22 @@
 package com.campbackend;
-import java.util.*;
+import java.time.LocalDate;
+import java.time.Period;
 public class Main {
 public static void main(String[] args) {
-    int leftLimit = 48; // numeral '0'
-    int rightLimit = 122; // letter 'z'
-    int targetStringLength = 20;
-    Random random = new Random();
+    // int leftLimit = 48; // numeral '0'
+    // int rightLimit = 122; // letter 'z'
+    // int targetStringLength = 20;
+    // Random random = new Random();
 
-    String generatedString = random.ints(leftLimit, rightLimit + 1)
-      .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
-      .limit(targetStringLength)
-      .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-      .toString();
+    // String generatedString = random.ints(leftLimit, rightLimit + 1)
+    //   .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+    //   .limit(targetStringLength)
+    //   .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+    //   .toString();
 
-    System.out.println(generatedString);
+    // System.out.println(generatedString);
+    LocalDate userDate=LocalDate.of(2020, 5, 4);
+    Period period=Period.between(userDate, LocalDate.now());
+    System.out.println(period.getYears());
 }
 }
