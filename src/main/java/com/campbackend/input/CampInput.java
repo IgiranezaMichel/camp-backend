e2eef538c4daf9c804470f0f83ed1044d5c30429
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-15 20:56:43 +0200

    add campInput

---
+++ src/main/java/com/campbackend/input/CampInput.java
@@ -0,0 +1,9 @@
+package com.campbackend.input;
+
+import com.campbackend.modal.Camp;
+
+import lombok.Getter;
+import lombok.Setter;
+public class CampInput extends Camp{
+@Getter @Setter private String  levelArr;
+}
