babdd453e043e34fd9d624ce9b85d92bbc477475
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-15 20:58:39 +0200

    add query to retrieve levels btn user age

--- src/main/java/com/campbackend/repository/LevelRepository.java
+++ src/main/java/com/campbackend/repository/LevelRepository.java
@@ -2,10 +2,14 @@ package com.campbackend.repository;
 
 import java.util.UUID;
 
+import java.util.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.campbackend.modal.Levels;
 
 public interface LevelRepository extends JpaRepository<Levels,UUID>{
+    @Query("select u from Levels u where u.fromAge<=:age AND u.toAge>=:age")
+    List<Levels> findLevelsBtnAge(int age);
 
 }
