3abc9d9ebc546975e7daf216906f3f40ba856793
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-15 20:58:03 +0200

    add findAll levels

--- src/main/java/com/campbackend/repository/BookRepository.java
+++ src/main/java/com/campbackend/repository/BookRepository.java
@@ -1,9 +1,14 @@
 package com.campbackend.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.*;
 import com.campbackend.modal.Book;
+import com.campbackend.modal.Levels;
 
 public interface BookRepository extends JpaRepository<Book, UUID> {
+
+    Page<Book> findAllByLevels(Levels levels, PageRequest of);
 
 }
