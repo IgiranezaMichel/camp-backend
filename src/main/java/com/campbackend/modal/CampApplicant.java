f0f815667513feefede4d37addb4c8ad9d35df4b
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-17 17:31:44 +0200

    add new props

--- src/main/java/com/campbackend/modal/CampApplicant.java
+++ src/main/java/com/campbackend/modal/CampApplicant.java
@@ -1,5 +1,15 @@
 package com.campbackend.modal;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.annotations.UuidGenerator.Style;
+
+import com.campbackend.enums.CampApplicantStatus;
+
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -7,31 +17,39 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.util.*;
-import org.hibernate.annotations.UuidGenerator;
-import org.hibernate.annotations.UuidGenerator.Style;
 
-import com.campbackend.enums.CampApplicantStatus;
-
-import java.time.*;
 @Data
 @NoArgsConstructor
 @Entity
 public class CampApplicant {
-@Id @UuidGenerator(style =Style.AUTO)
-private UUID id;
-@ManyToOne(cascade = CascadeType.ALL,targetEntity = Camp.class)
-private Camp camp;
-@ManyToOne(cascade = CascadeType.ALL,targetEntity = AccountHolder.class)
-private AccountHolder accountHolder;
-@Enumerated(EnumType.STRING)
-private CampApplicantStatus campApplicantStatus;
-private LocalDate timeStamp;
-public CampApplicant(UUID id, Camp camp, AccountHolder accountHolder, LocalDate timeStamp,CampApplicantStatus campApplicantStatus) {
-    this.id = id;
-    this.camp = camp;
-    this.accountHolder = accountHolder;
-    this.timeStamp = timeStamp;
-    this.campApplicantStatus=campApplicantStatus;
-}
+    @Id
+    @UuidGenerator(style = Style.AUTO)
+    private UUID id;
+    @ManyToOne(cascade = CascadeType.ALL, targetEntity = Camp.class)
+    private Camp camp;
+    @ManyToOne(cascade = CascadeType.ALL, targetEntity = AccountHolder.class)
+    private AccountHolder accountHolder;
+    @ManyToOne(cascade = CascadeType.ALL, targetEntity = Levels.class)
+    private Levels levels;
+    @Enumerated(EnumType.STRING)
+    private CampApplicantStatus campApplicantStatus;
+    private String paymentCode;
+    private String telephone;
+    @Column(columnDefinition = "text")
+    private String comment;
+    private LocalDate timeStamp;
+
+    public CampApplicant(UUID id, Camp camp, AccountHolder accountHolder, LocalDate timeStamp,
+            CampApplicantStatus campApplicantStatus, Levels levels, String telephone, String paymentCode,
+            String comment) {
+        this.id = id;
+        this.camp = camp;
+        this.accountHolder = accountHolder;
+        this.timeStamp = timeStamp;
+        this.campApplicantStatus = campApplicantStatus;
+        this.setLevels(levels);
+        this.telephone = telephone;
+        this.comment = comment;
+        this.paymentCode = paymentCode;
+    }
 }
