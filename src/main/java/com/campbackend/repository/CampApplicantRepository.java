faadcf2b48654004660554988fc91653fdf775df
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-17 17:32:14 +0200

    add find all by camp

--- src/main/java/com/campbackend/repository/CampApplicantRepository.java
+++ src/main/java/com/campbackend/repository/CampApplicantRepository.java
@@ -6,10 +6,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.*;
 
 import com.campbackend.modal.AccountHolder;
+import com.campbackend.modal.Camp;
 import com.campbackend.modal.CampApplicant;
 
 public interface CampApplicantRepository extends JpaRepository<CampApplicant,UUID>{
 
     Page<CampApplicant> findAllByAccountHolder(AccountHolder accountHolder, PageRequest of);
+
+    Page<CampApplicant> findAllByCamp(Camp camp, PageRequest of);
+
+    CampApplicant findByAccountHolderAndCamp(AccountHolder accountHolder, Camp camp);
 
 }
