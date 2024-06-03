ec1b8f9217dbf4354f6d4b54c5855b1f5df0b969
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-06-03 19:24:16 +0200

    add AllArgsConstructor

--- src/main/java/com/campbackend/modal/Exam.java
+++ src/main/java/com/campbackend/modal/Exam.java
@@ -5,19 +5,26 @@ import java.util.UUID;
 import org.hibernate.annotations.UuidGenerator;
 import org.hibernate.annotations.UuidGenerator.Style;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Exam {
 @Id @UuidGenerator(style = Style.AUTO)
 private UUID id;
 private String name;
+private String description;
 @Column(columnDefinition = "text")
 private String instruction;
+@ManyToOne(cascade = CascadeType.ALL,targetEntity = AyGrade.class)
+private AyGrade ayGrade;
 }
