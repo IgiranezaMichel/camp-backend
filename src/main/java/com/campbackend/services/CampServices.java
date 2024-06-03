4234453f9d27d763b1540137e3e36a9a686dacc3
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-06-03 19:26:09 +0200

    saveOrUpdateCamp

--- src/main/java/com/campbackend/services/CampServices.java
+++ src/main/java/com/campbackend/services/CampServices.java
@@ -21,17 +21,25 @@ import java.time.*;
 public class CampServices {
     @Autowired
     private CampRepository campRepository;
-    // @Autowired
-    // private LevelServices levelServices;
     public ResponseEntity<String> saveOrUpdateCamp(CampInput camp) {
+     
         try {
-            String ids=camp.getLevelArr();
-            System.out.println(ids.split("["));
             camp.setTimeStamp(LocalDateTime.now());
-            Camp data = campRepository.save(camp);
+            if (camp.getStartingDate().isBefore(LocalDate.now())) {
+                throw new Exception("Please Specify the date before today");
+            } else if (camp.getStartingDate().isAfter(camp.getEndingDate())) {
+                throw new Exception("Please Specify the date before " + camp.getStartingDate());
+            } else if (camp.getDeadline().isBefore(LocalDate.now())) {
+                throw new Exception("Please Specify the date before " + LocalDate.now());
+            } else if (camp.getCost() == 0) {
+                throw new Exception("Please Specify Cost");
+            }
+            Camp data = campRepository
+                    .save(new Camp(camp.getId(), camp.getTitle(), camp.getCost(), camp.getAddress(), camp.getDeadline(),
+                            camp.getStartingDate(), camp.getEndingDate(), camp.getDescription(), camp.getContent()));
             return new ResponseEntity<>(data.getTitle() + " saved successful", HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>(" Camp already exist", HttpStatus.METHOD_NOT_ALLOWED);
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.METHOD_NOT_ALLOWED);
         }
     }
 
@@ -64,7 +72,7 @@ public class CampServices {
     }
 
     public CampPage activeCamp(PageInput page) {
-        org.springframework.data.domain.Page<Camp> pagination = campRepository.findAllByEndingDateAfter(LocalDate.now(),
+        org.springframework.data.domain.Page<Camp> pagination = campRepository.findAllByDeadlineAfter(LocalDate.now(),
                 PageRequest.of(page.getPageNumber(), page.getPageSize(), Sort.by(page.getSort())));
         return new CampPage(pagination.getNumber(), pagination.getTotalPages(), pagination.getTotalElements(),
                 pagination.getContent());
