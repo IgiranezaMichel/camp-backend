fcfa26fd276b25ec8b20cd03f67bbb5077c92c76
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-06-06 19:22:47 +0200

    add coockies

--- src/main/java/com/campbackend/controller/IndexController.java
+++ src/main/java/com/campbackend/controller/IndexController.java
@@ -1,57 +1,50 @@
 package com.campbackend.controller;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.bind.annotation.RestController;
 
+import com.campbackend.dto.AccountHolderDTO;
 import com.campbackend.modal.AccountHolder;
-import com.campbackend.repository.AccountHolderRepository;
+import com.campbackend.services.AccountHolderServices;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+
+import java.security.Principal;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 @RestController
-@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+@Slf4j
 public class IndexController {
-  @Autowired AccountHolderRepository aServices;
-    @RequestMapping(value="/login")
-    public ResponseEntity<Object>  login()
-    {
-      Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-      String email=authentication.getName();
-      AccountHolder accountHolder=aServices.findByEmail(email);
-      return authentication.isAuthenticated()
-      ?
-      new ResponseEntity<>(accountHolder,HttpStatus.OK)
-      :
-      new ResponseEntity<>("Wrong username or password",HttpStatus.UNAUTHORIZED);
-     }
-     @RequestMapping(value="/login-success")
-     public ResponseEntity<Object>  logIn(@RequestParam Object parameter)
-     {
-      System.out.println(parameter);
-       Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-       String email=authentication.getName();
-       AccountHolder accountHolder=aServices.findByEmail(email);
-       return authentication.isAuthenticated()
-       ?
-       new ResponseEntity<>(accountHolder,HttpStatus.OK)
-       :
-       new ResponseEntity<>("Wrong username or password",HttpStatus.UNAUTHORIZED);
-      }
-     @RequestMapping(value="/success-login")
-     public ResponseEntity<Object>  logInSuccess()
-     {
-       Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-       String email=authentication.getName();
-       AccountHolder accountHolder=aServices.findByEmail(email);
-       return authentication.isAuthenticated()
-       ?
-       new ResponseEntity<>(accountHolder,HttpStatus.OK)
-       :
-       new ResponseEntity<>("Wrong username or password",HttpStatus.UNAUTHORIZED);
-      }
-      
-     
+  @Autowired private AccountHolderServices accountHolderServices;
+@RequestMapping("/login")
+public String requestMethodName(HttpServletRequest rServletRequest,HttpServletResponse response,HttpSession session) {
+  Authentication auth=SecurityContextHolder.getContext().getAuthentication();
+  log.info("servlet request {}",rServletRequest.getAuthType());
+  log.info("authentication {}",auth.isAuthenticated());
+  log.info("Email {}",auth.getName());
+   log.info("user detail {}",auth.getDetails());
+   log.info("session {}",rServletRequest.getSession().getId());
+  if(!auth.getName().equals("anonymous")){
+    Cookie cookie=new Cookie("home", "null");
+    response.addCookie(cookie);
+  }
+ 
+    return new String();
+}
+@PostMapping(value = "/success-login")
+public AccountHolderDTO getMethodName(Principal principal,HttpServletRequest session) {
+  AccountHolder accountHolder=accountHolderServices.findByEmail(principal.getName());
+  log.info("Session generated",session.getSession().getId());
+    return new AccountHolderDTO(accountHolder.getId(), accountHolder.getName(), accountHolder.getGender(), accountHolder.getDob(), accountHolder.getPhoneNumber(), accountHolder.getEmail(), accountHolder.getProfilePicture(), accountHolder.getRole());
+}
+
 }
